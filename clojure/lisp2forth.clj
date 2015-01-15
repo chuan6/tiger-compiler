d16ab6a681a93226b8d8ef1831df415f6da62fd0
chuan6 <chuan6.dev@gmail.com>
2015-01-15 10:10:52 -0600

    remove forth "compiler"

--- clojure/lisp2forth.clj
+++ clojure/lisp2forth.clj
@@ -1,5 +1,4 @@
-(ns forth
-  (require [clojure.edn :as edn]))
+(ns forth)
 
 (defn triplet
   ([list] (triplet () list 0))
@@ -23,32 +22,3 @@
 (defn to-forth [s]
   (flatten (norm s)))
 
-(defn lisp-to-forth [s]
-  (eval-forth (to-forth s)))
-
-(defn execute-symbol
-  "Use `symbol` to perform an operation. Returns a vector
-  of the new input and stack, intended to be applied to `eval-forth`."
-  [symbol input stack]
-  (let [a (first stack)
-        b (second stack)
-        nu-stack (rest (rest stack))
-        nu-input (rest input)]
-    (case symbol
-      + [nu-input (cons (+ a b) nu-stack)]
-      - [nu-input (cons (- a b) nu-stack)]
-      * [nu-input (cons (* a b) nu-stack)])))
-
-(defn eval-forth
-  ([input] (eval-forth input []))
-  ([input stack]
-   ;; When the input is done the answer is TOS
-   (if (empty? input)
-     stack
-     ;; Otherwise, read the first symbol...
-     (let [first-symbol (first input)]
-       (if (= (class first-symbol) clojure.lang.Symbol)
-         ;; Execute symbols
-         (apply eval-forth (execute-symbol first-symbol input stack))
-         ;; Push everything else, hoping they're numerics.
-         (eval-forth (rest input) (cons first-symbol stack)))))))
