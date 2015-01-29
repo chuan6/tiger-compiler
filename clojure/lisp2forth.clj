fe81e2232aec5d6553b731c4835b1f3332ae514c
chuan6 <chuan6.dev@gmail.com>
2015-01-29 00:27:13 -0600

    uses Clojure destructing mechanism instead of first-triplet

--- clojure/lisp2forth.clj
+++ clojure/lisp2forth.clj
@@ -1,28 +1,15 @@
 (ns forth)
 
-;; split a list into two parts, the first triplet of the list, and the rest.
-;; e.g., from (+ 1 2 3 4 5 6 7) to ((+ 1 2) (3 4 5 6 7))
-(defn first-triplet
-  ([ts] (first-triplet () ts 0))
-  ([t ts count]
-   (if (= count 3)
-     `(~(reverse t) ~ts)
-     (first-triplet (conj t (first ts))
-                    (rest ts)
-                    (inc count)))))
-
 ;; e.g., from (* (+ 1 2) 3 4) to (4 (3 (2 1 +) *) *)
 (defn norm [s]
   (if (or (number? s) (= (class s) clojure.lang.Symbol))
     s
-    (if (list? s)
+    (if (seq? s)
       (if (<= (count s) 3) ;TODO inefficiency: need to go through list everytime
         (reverse (map norm s))
-        (let [[tri tri-rest] (first-triplet s)]
-          (norm (-> tri-rest
-                    (conj tri)
-                    (conj (first tri))))))
-      (println "Error: expect input to be a lisp expression, i.e., a list."))))
+        (let [[op a b & more] s]
+          (norm (conj more `(~op ~a ~b) op))))
+      (println "Error: expect input to be a lisp expression, i.e., a list." (type s)))))
 
 (defn to-forth [s]
   (flatten (norm s)))
