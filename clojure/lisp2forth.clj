d30bce70fe5320d01af90793ff283ab5346d681c
chuan6 <chuan6.dev@gmail.com>
2015-02-04 18:15:14 -0600

    use (test n) to test for n cases.

--- clojure/lisp2forth.clj
+++ clojure/lisp2forth.clj
@@ -1,6 +1,8 @@
 (ns forth)
 
-;; e.g., from (* (+ 1 2) 3 4) to (4 (3 (2 1 +) *) *)
+;;;Usage example: (to-forth '(* (+ 1 2) 3 4))
+
+;;e.g., from (* (+ 1 2) 3 4) to (4 (3 (2 1 +) *) *)
 (defn norm [s]
   (if (or (number? s) (= (class s) clojure.lang.Symbol))
     s
@@ -14,7 +16,7 @@
 (defn to-forth [s]
   (flatten (norm s)))
 
-;; take the current stack, and reduce it with the next symbol
+;;Take the current stack, and reduce it with the next symbol.
 (defn forth-reducer [stack next-symbol]
   (if (contains? #{'+ '- '* '/} next-symbol)
     (let [stack'    (pop  stack)
@@ -24,7 +26,43 @@
       (conj unchanged (eval `(~next-symbol ~arg1 ~arg2))))
     (conj stack next-symbol)))
 
-;;return result of a forth calculation expression
+;;Return result of a forth calculation expression.
 (defn forth-calculator [s]
   (let [init-stack ()]
-    (reduce forth-reducer init-stack s)))
+    (peek (reduce forth-reducer init-stack s))))
+
+(defn gen-test-op []
+  (let [opvec ['+ '- '* '/]
+        n     (count opvec)]
+    (nth opvec (rand-int n))))
+
+(defn gen-test-cell [max]
+  (assert (>= max 2))
+  (if (= max 2)
+    `(~(gen-test-op) ~(rand-int 1000) ~(rand-int 1000))
+    (loop [c     max
+           built `(~(gen-test-op))]
+      (if (= c 0)
+        (reverse built)
+        (recur (- c 1) (conj built (gen-test-cell (- max 1))))))))
+
+(defn gen-test []
+  (let [case    (gen-test-cell 4) ;Note: 4 here is large enough
+        clojure (eval case)
+        forth   (forth-calculator (to-forth case))]
+    (println clojure "; clojure result")
+    (println forth "; forth result")
+    (= clojure forth)))
+
+(defn test [n]
+  (assert (>= n 0))
+  (loop [success? true
+         nmore    n
+         m        0]
+    (if (= nmore 0)
+      (do (println n "test cases run;" m "failed.")
+          success?)
+      (let [result (gen-test)]
+        (recur (and result success?)
+               (dec nmore)
+               (if result m (inc m)))))))
