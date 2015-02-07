5407a63de02ecff7ba39fff80e83e9ddfe6e83b7
chuan6 <chuan6.dev@gmail.com>
2015-02-07 13:48:13 -0600

    follow-set works on sample, using magic ff function :)

--- clojure/cfg.clj
+++ clojure/cfg.clj
@@ -2,18 +2,22 @@
   (:require [clojure.set :as set]))
 
 (def empty-string 'ε)
-(def end-mark '$)
+(def end-marker '$)
 
 (def sample
   {:terminal #{\+ \* \( \) 'id} ;TODO decide if add empty-string here
+   :start 'expr
    :production
-   {'expr        [['term 'expr-helper]]
+   {'expr        [['term 'expr-helper]] ;first as production for the start symbol
     'expr-helper [[\+ 'term 'expr-helper]   empty-string]
     'term        [['factor 'term-helper]]
     'term-helper [[\* 'factor 'term-helper] empty-string]
-    'factor      [[\( 'expr \)] ['id]]
-    } ;number is index in :terminal
-   })
+    'factor      [[\( 'expr \)] ['id]]}})
+
+(defn terminal? [grammar x]
+  (if (= x empty-string)
+    true
+    (contains? (:terminal grammar) x)))
 
 (declare first-set-non-term)
 
@@ -23,7 +27,7 @@
     (if (empty? p)
       s
       (let [x (first p)]
-        (if (contains? (:terminal g) x)
+        (if (terminal? g x)
           (conj s x)
           (let [r (first-set-non-term g x)
                 ss (set/union s r)]
@@ -44,7 +48,78 @@
 (defn first-set [grammar string]
   (first-set-production grammar string #{}))
 
-(defn follow-set [])
+;(def init-follow-set
+ ; {'expr #{} 'expr-helper #{}
+  ; 'term #{} 'term-helper #{}
+   ;'factor #{}})
 
+(defn init-follow-set [grammar]
+  (let [p (seq (:production grammar))]
+    (loop [result {}
+           ps     p]
+      (if (empty? ps)
+        result
+        (let [nt (nth (first ps) 0)]
+          (recur (assoc result nt
+                        (if (= nt (:start grammar))
+                          #{end-marker} #{}))
+                 (rest ps)))))))
+
+(defn init-data [grammar]
+  {:set (init-follow-set grammar)
+   :infer (fn [x] x)})
+
+(defn non-terminal? [grammar x]
+  (let [ps (:production grammar)]
+    (not (nil? (get ps x)))))
+
+(defn add-to-follow-set [data nt new]
+  (assoc data nt (set/union (get data nt)
+                            (disj new empty-string))))
+
+(defn iterate-til-fixed [f x]
+  (let [x' (f x)]
+    (if (= x' x) x (iterate-til-fixed f x')))) ;powerful '=' operation!
+
+(defn ff [f x y]
+  (fn [s] (let [s (f s)]
+               (assoc s y (set/union (get s x) (get s y))))))
+
+(defn follow-set-production [grammar left right data]
+  (loop [right right
+         data data]
+    (if (empty? right)
+      data
+      (let [x (first right)
+            xs (rest right)]
+        (cond
+          (terminal? grammar x) (recur xs data)
+          (non-terminal? grammar x)
+          (let [x-next (first-set grammar xs)
+                curr-set (add-to-follow-set (:set data) x x-next)]
+              (if (or (empty? x-next) (contains? x-next empty-string))
+                (recur xs
+                       {:set curr-set :infer (ff (:infer data) left x)})
+                (recur xs
+                       {:set curr-set :infer (:infer data)}))))))))
+
+(defn follow-set [grammar]
+  (let [prods (seq (:production grammar))]
+    (loop [prods prods
+           data (init-data grammar)]
+      (if (empty? prods)
+        (iterate-til-fixed (:infer data) (:set data))
+        (recur (rest prods)
+               (let [prod (first prods)
+                     left (nth prod 0)]
+                 (loop [alts (nth prod 1)
+                        data data]
+                   (if (empty? alts)
+                     data
+                     (let [right (first alts)]
+                       (if (= right empty-string)
+                         (recur (rest alts) data)
+                         (recur (rest alts)
+                                (follow-set-production grammar left right data))))))))))))
 
 
