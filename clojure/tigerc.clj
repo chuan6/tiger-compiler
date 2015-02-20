9c209e6a5e3b202565fa875add49de747f07e4da
chuan6 <chuan6.dev@gmail.com>
2015-02-20 01:40:20 -0600

    tigerc.clj: tiger tokenizer v0
    Usage: (tokenize-file "../sample.tiger")
    
    - fixed a bug in string-recognizer
    - now read from a tiger source code file
    - now print queue nicely

--- clojure/tigerc.clj
+++ clojure/tigerc.clj
@@ -11,7 +11,7 @@
 (def token-punct
   #{'comma 'colon 'semi-colon 'open-paren 'close-paren
     'open-bracket 'close-bracket 'open-brace 'close-brace
-    'period 'plus 'minus 'star 'slash 'equal 'angle 'lt
+    'period 'plus 'minus 'star 'slash 'equal 'diamond 'lt
     'leq 'gt 'geq 'and 'pipe 'assign})
 
 (def token-set
@@ -20,6 +20,11 @@
                      token-punct))
 
 (def token-queue clojure.lang.PersistentQueue/EMPTY)
+;;print-method implementation is from Joy of Clojure(2nd edition)
+(defmethod print-method clojure.lang.PersistentQueue [queue writer]
+  (print-method '<- writer)
+  (print-method (seq queue) writer)
+  (print-method '-< writer))
 
 (defn tree-insert [tree key comp-fn]
   (if tree
@@ -63,6 +68,19 @@
               {:char-seq s :token-seq (conj (:token-seq curr)
                                             {:token 'id :name token})})))))))
 
+(defn digits-recognizer [curr]
+  (let [s (:char-seq curr)
+        c (first s)]
+    (assert (Character/isDigit c))
+    (loop [s (rest s)
+           t [c]]
+      (let [c (first s)]
+        (if (and c (Character/isDigit c))
+          (recur (rest s) (conj t c))
+          {:char-seq s :token-seq (conj (:token-seq curr)
+                                        {:token 'digits
+                                         :value (str/join t)})})))))
+
 (defn string-recognizer [curr]
   (assert (= (first (:char-seq curr)) \"))
   (let [s (rest (:char-seq curr))
@@ -80,7 +98,7 @@
                 peek (second s) ;c and peek are non-nil
                 cbc  (if (= c \\) (inc consecutive-backslash-count)
                          0)]
-            (if (and (= peek \") (even? consecutive-backslash-count))
+            (if (and (= peek \") (even? cbc))
               {:char-seq (rest (rest s))
                :token-seq (conj token-seq
                                 {:token 'string :value (str/join (conj t c))})}
@@ -100,7 +118,7 @@
             (assoc curr :char-seq ())) ;the problematic token is not recorded
         (let [c   (first s)
               suc (second s)]
-          (cond (and (= c \*) (= suc \/))
+          (cond (and (= c \*) (= suc \/)) ;closing
                 (let [flag-count (dec flag-count)]
                   (if (= flag-count 0)
                     {:char-seq (rest (rest s))
@@ -110,7 +128,7 @@
                            (-> t (conj c) (conj suc))
                            flag-count)))
 
-                (and (= c \/) (= suc \*))
+                (and (= c \/) (= suc \*)) ;openning
                 (let [flag-count (inc flag-count)]
                   (recur (rest (rest s)) ;note pattern /*/
                          (-> t (conj c) (conj suc))
@@ -118,5 +136,75 @@
 
                 :else (recur (rest s) (conj t c) flag-count)))))))
 
-(defn tokenize [in]
-  )
+(defn skip-spaces [curr]
+  (let [s (:char-seq curr)
+        c (first s)]
+    (assert (Character/isWhitespace c))
+    (loop [s (rest s)]
+      (let [c (first s)]
+        (if (and c (Character/isWhitespace c))
+          (recur (rest s))
+          (assoc curr :char-seq s))))))
+
+(defn tokenize-str [s]
+  (assert (string? s))
+  (let [add-punct (fn [curr sym forward]
+                    {:char-seq (nthrest (:char-seq curr) forward)
+                     :token-seq (conj (:token-seq curr) {:token sym})})]
+    (loop [curr {:char-seq s :token-seq token-queue}]
+      (let [s (:char-seq curr)
+            q (:token-seq curr)]
+        (if (empty? s)
+          (:token-seq curr)
+          (let [c   (first s)
+                suc (second s)] ;csuc may be nil
+            (cond
+              (Character/isWhitespace c) ;skip leading spaces
+              (recur (skip-spaces curr))
+              
+              (Character/isLetter c)     ;find an 'id, or keyword token
+              (recur (id-recognizer curr))
+
+              (= c \")                   ;find a 'string token
+              (recur (string-recognizer curr))
+
+              (Character/isDigit c)      ;find a 'digits token
+              (recur (digits-recognizer curr))
+
+              (= c \/)                   ;find a 'comment, or a 'slash token
+              (if (and suc (= suc \*))
+                (recur (comment-recognizer curr))
+                (recur {:char-seq (rest s)
+                        :token-seq (conj q {:token 'slash})}))
+
+              :else
+              (case c
+                \, (recur (add-punct curr 'comma 1))
+                \: (if (and suc (= suc \=))
+                     (recur (add-punct curr 'assign 2))
+                     (recur (add-punct curr 'colon 1)))
+                \; (recur (add-punct curr 'semi-colon 1))
+                \( (recur (add-punct curr 'open-paren 1))
+                \) (recur (add-punct curr 'close-paren 1))
+                \[ (recur (add-punct curr 'open-bracket 1))
+                \] (recur (add-punct curr 'close-bracket 1))
+                \{ (recur (add-punct curr 'open-brace 1))
+                \} (recur (add-punct curr 'close-brace 1))
+                \. (recur (add-punct curr 'period 1))
+                \+ (recur (add-punct curr 'plus 1))
+                \- (recur (add-punct curr 'minus 1))
+                \* (recur (add-punct curr 'star 1))
+                \= (recur (add-punct curr 'equal 1))
+                \< (cond (and suc (= suc \>)) (recur (add-punct curr 'diamond 2))
+                         (and suc (= suc \=)) (recur (add-punct curr 'leq 2))
+                         :else (recur (add-punct curr 'lt 1)))
+                \> (if (and suc (= suc \=)) (recur (add-punct curr 'geq 2))
+                       (recur (add-punct curr 'gt 1)))
+                \& (recur (add-punct curr 'and 1))
+                \| (recur (add-punct curr 'pipe 1))
+                (:token-seq curr)))))))))
+
+(defn tokenize-file [path-to-file]
+  (let [str (try (slurp path-to-file)
+                 (catch Exception e (.getMessage e)))]
+    (tokenize-str str)))
