bb296c2221259511eb469caaeffe553a090f8ee0
chuan6 <chuan6.dev@gmail.com>
2016-05-21 11:11:46 -0500

    project directory restructure [unfinished]

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tigerc "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "GNU GPL v2.0"
+            :url "http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
